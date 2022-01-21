a73983cf975505d2cbae17582cf1d11bfe9bf635
Joakim RN <joakimrn02@gmail.com>
2022-01-21 19:17:33 +0100

    Commit, because

--- app/deploy_docker.sh
+++ app/deploy_docker.sh
@@ -1,1 +1,2 @@
-sudo docker build -t fast_api_playground . && sudo docker run -d --name Fast_api_playground -p 80:80 fast_api_playground
+sudo docker build -t fast_api_playground . && sudo docker run -d --name Fast_api_playground -p 8000:80 fast_api_playground
+
